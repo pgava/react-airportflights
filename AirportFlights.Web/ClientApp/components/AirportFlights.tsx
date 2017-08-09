c167f0ce55ca53a2a315f7afc024cd43a147fe21
Paolo <paolo.gava@gmail.com>
2017-08-09 23:07:49 +1000

    Refactor Airport component

--- AirportFlights.Web/ClientApp/components/AirportFlights.tsx
+++ AirportFlights.Web/ClientApp/components/AirportFlights.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { ApplicationState } from '../store';
 import * as AirportFlightsStore from '../store/AirportFlights';
+import AirportGatesTable from './AirportGatesTable';
 
 type AirportFlightsProps = AirportFlightsStore.AirportFlightsState & typeof AirportFlightsStore.actionCreators;
 
@@ -12,55 +13,18 @@ class AirportFlights extends React.Component<AirportFlightsProps, void> {
     componentWillReceiveProps(nextProps) {
     }
 
-    handleUpdate(flight) {        
-        this.props.goToFlight(flight.flightId);         
+    handleUpdate = (flight) => {
+        this.props.goToFlight(flight.flightId);
     }
 
     public render() {
         return <div>
-            <h1></h1>
-            {this.renderGateTable()}
+            <AirportGatesTable
+                gates={this.props.gates}
+                onUpdate={this.handleUpdate} />
         </div>;
-    }
-
-    renderGateTable() {
-        return <div className="col-md-12">
-               {this.props.gates.map(gate =>
-                <div className="well well-lg" key={gate.gateId}>
-                    <h2>{gate.gateName}</h2>
-                        {this.renderFlightTable(gate.flights)}
-                </div>
-                )}
-        </div>;
-    }
-
-    renderFlightTable(flights) {
-        return <table className="table table-striped">
-                   <thead>
-                   <tr>
-                       <th>Name</th>
-                       <th>Arrive</th>
-                       <th>Leave</th>
-                       <th></th>
-                   </tr>
-                   </thead>
-                   <tbody>
-                   {flights.map(flight =>
-                        <tr key={flight.flightId}>
-                        <td>{flight.flightNumber}</td>
-                        <td>{flight.arrival}</td>
-                        <td>{flight.departure}</td>
-                        <td>
-                            <button type="button" onClick={() => this.handleUpdate(flight)
-                            } className="btn btn-info btn-sm">Update</button>
-                        </td>
-                    </tr>
-                   )}
-                   </tbody>
-               </table>;
     }
 }
-                     
 
 export default connect(
     (state: ApplicationState) => {
---
+++ AirportFlights.Web/ClientApp/components/AirportGatesTable.tsx
@@ -0,0 +1,49 @@
+﻿import * as React from 'react';
+
+const AirportFlightsTable = ({ flights, onUpdate }) => {
+    return <table className="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Arrive</th>
+                <th>Leave</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {flights.map(flight =>
+                    <tr key={flight.flightId}>
+                        <td>{flight.flightNumber}</td>
+                        <td>{flight.arrival}</td>
+                        <td>{flight.departure}</td>
+                        <td>
+                            <button type="button" onClick={() => onUpdate(flight)
+                            } className="btn btn-info btn-sm">Update</button>
+                        </td>
+                    </tr>
+                )}         
+        </tbody>
+    </table>;
+};
+
+const AirportGatesTable = ({ gates, onUpdate }) => {
+    return (
+        <div>
+            <h1></h1>
+            <div className="col-md-12">
+                {gates.map(gate =>
+                    <div className="well well-lg" key={gate.gateId}>
+                        <h2>{gate.gateName}</h2>
+                        <AirportFlightsTable
+                            flights={gate.flights}
+                            onUpdate={onUpdate} />
+                    </div>
+                )}
+            </div>;
+        </div>
+        );
+};
+
+export default AirportGatesTable;
+
+
